1deaa1a6bb40d1fb02d2371077a8d977a0ca7dfc
Dário Silva <dariosilva13222@gmail.com>
2024-09-16 21:19:27 +0100

    Creates SpringBoot Project

---
+++ src/test/java/ao/com/academy/studentsapi/StudentsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.com.academy.studentsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
