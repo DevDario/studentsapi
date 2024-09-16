c5c626e05f0a7ed98270b1dc2bd9335e0252d615
Dário Silva <dariosilva13222@gmail.com>
2024-09-16 21:48:25 +0100

    refactor: add lombok's getter and setter annotations

--- src/main/java/ao/com/academy/studentsapi/domain/Student.java
+++ src/main/java/ao/com/academy/studentsapi/domain/Student.java
@@ -1,31 +1,14 @@
 package ao.com.academy.studentsapi.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Student {
     private String name;
     private String email;
     private Long id;
-    
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
