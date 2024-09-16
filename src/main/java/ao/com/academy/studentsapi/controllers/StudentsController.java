b0353f6624c7edeae8007ca8a36a388d021fee20
Dário Silva <dariosilva13222@gmail.com>
2024-09-17 00:32:51 +0100

    feat: updates a student based on his ID

--- src/main/java/ao/com/academy/studentsapi/controllers/StudentsController.java
+++ src/main/java/ao/com/academy/studentsapi/controllers/StudentsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ao.com.academy.studentsapi.domain.Student;
 import ao.com.academy.studentsapi.exceptions.StudentNotFoundException;
 import ao.com.academy.studentsapi.repository.StudentsRepository;
+
 
 
 
@@ -42,6 +44,19 @@ public class StudentsController {
         return studentsRepository.findById(studentID).orElseThrow(() -> new StudentNotFoundException(studentID));
     }
     
+    // Update Student
+    @PutMapping("update/student/{id}")
+    public Student updateStudent(@PathVariable(value="id") Long studentID, @RequestBody Student studentDetails) throws StudentNotFoundException {
+
+        Student student = studentsRepository.findById(studentID).orElseThrow(() -> new StudentNotFoundException(studentID));
+
+        student.setName(studentDetails.getName());
+        student.setEmail(studentDetails.getEmail());
+
+        Student newStudent = studentsRepository.save(student);
+
+        return newStudent;
+    }
     
     
 
