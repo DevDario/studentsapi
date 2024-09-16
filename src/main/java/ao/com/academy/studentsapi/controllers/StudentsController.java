36572b361dbe3fcf057d4a647ff30e688c0f5c3c
Dário Silva <dariosilva13222@gmail.com>
2024-09-16 23:46:18 +0100

    raw: creates Get and Post Mappings

--- src/main/java/ao/com/academy/studentsapi/controllers/StudentsController.java
+++ src/main/java/ao/com/academy/studentsapi/controllers/StudentsController.java
@@ -1,10 +1,37 @@
 package ao.com.academy.studentsapi.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ao.com.academy.studentsapi.domain.Student;
+import ao.com.academy.studentsapi.repository.StudentsRepository;
+
 
 @RestController
 @RequestMapping("/controller")
 public class StudentsController {
     
+    @Autowired
+    StudentsRepository studentsRepository;
+
+    //Get All Students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return studentsRepository.findAll();
+    }
+
+    //Create new student
+    @PostMapping("/new/student")
+    public void createStudent(@RequestBody Student student) {
+        studentsRepository.save(student);
+    }
+    
+    
+
 }
